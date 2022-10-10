d076d0b7b78e65cc8dcfd15c1573062de1a0f4e9
Kulunchick <nikolikrasovski@gmail.com>
2022-10-11 01:49:58 +0300

    main

--- handlers/inPrivate.py
+++ handlers/inPrivate.py
@@ -1,9 +1,13 @@
 from aiogram import types, Router
-from messages import *
+from messages.inGroup import *
 from aiogram.filters import Command
 
 router = Router()
 
 @router.message(Command(commands=["start"]))
+async def cmd_send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+@router.message(Command(commands=["help"]))
 async def cmd_send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
