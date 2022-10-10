8079f0d1e32c3c0ca6d4159ea6bc7cd29d2cc6d7
Kulunchick <nikolikrasovski@gmail.com>
2022-10-11 01:49:58 +0300

    main

--- handlers/inGroup.py
+++ handlers/inGroup.py
@@ -1,6 +1,6 @@
 from aiogram import types, Bot, Router, F
 from aiogram.utils.deep_linking import create_start_link, decode_payload
-from messages import *
+from messages.inGroup import *
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 router = Router()
@@ -8,7 +8,7 @@ router = Router()
 @router.message(F.content_type.in_({types.ContentType.NEW_CHAT_MEMBERS})) #, types.ContentType.LEFT_CHAT_MEMBER
 async def check_channel(message: types.Message, bot: Bot):
     bot_id = (await bot.get_me()).id
-    link = await create_start_link(bot, payload = str(message.chat.id), encode=True)
+    link = await create_start_link(bot, payload = str(message.chat.id))
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
         text = "Нажми меня",
