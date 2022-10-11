9371b715267cca33a5134adf5fe3678ccaed6d61
Kulunchick <nikolikrasovski@gmail.com>
2022-10-11 19:55:06 +0300

    Add states

--- buttons.py
+++ buttons.py
@@ -1,7 +1,26 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from typing import List
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 async def generate_refferal_button(url: str):
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text = "➕ Додай свій день народження в календар", url=url)]
     ])
+
+async def get_month_keyboard(months: List[str]):
+    builder = ReplyKeyboardBuilder()
+    for i in months:
+        builder.add(KeyboardButton(text=str(i)))
+    builder.adjust(4)
+    return builder.as_markup(resize_keyboard=True)
+
+async def get_gender_keyboard(genders: List[str]):
+    builder = ReplyKeyboardBuilder()
+    builder.row(
+        KeyboardButton(text = genders[0]),
+        KeyboardButton(text = genders[1])
+    )
+    builder.row(
+        KeyboardButton(text = genders[2])
+    )
+    return builder.as_markup(resize_keyboard=True)
