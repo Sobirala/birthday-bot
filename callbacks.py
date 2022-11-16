6d4da2bb46d6a62d076ffa4c59dfc5e5bd4936de
Kulunchick <53000952+Kulunchick@users.noreply.github.com>
2022-11-16 13:31:55 +0200

    Revert "Merge branches"

--- callbacks.py
+++ callbacks.py
@@ -1,7 +1,6 @@
 from aiogram.filters.callback_data import CallbackData
-from typing import Union
 
 
 class NumbersCallbackFactory(CallbackData, prefix="fabnum"):
     action: str
-    value: Union[int, str]
+    value: int | str
