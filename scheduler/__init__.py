8db8d3c8529d43b68cd8661bc41a548d1474b0a9
Kulunchick <nikolikrasovski@gmail.com>
2022-11-29 15:42:36 +0200

    UPD: fix scheduler if user blocked bot

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -3,8 +3,8 @@ import logging
 from datetime import datetime, timedelta
 from typing import Any
 
-import aioschedule
 from aiogram import Bot
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from messages.sheduler import *
 
@@ -15,16 +15,29 @@ class Scheduler:
         self.database: Any = database
 
     async def start(self):
-        aioschedule.every().hour.at(":01").do(self.today)
-        aioschedule.every().hour.at(":01").do(self.tomorrow)
-        aioschedule.every().hour.at(":01").do(self.next_5_day)
-        asyncio.create_task(self.polling())
+        scheduler = AsyncIOScheduler()
+        scheduler.add_job(self.congratulations, 'cron', minute="01", hour="*")
+        scheduler.start()
 
-    @staticmethod
-    async def polling():
-        while True:
-            await aioschedule.run_pending()
-            await asyncio.sleep(1)
+    async def congratulations(self):
+        today = asyncio.create_task(self.today(), name="Today function")
+        tomorrow = asyncio.create_task(self.tomorrow(), name="Tomorrow function")
+        next_5_day = asyncio.create_task(self.next_5_day(), name="Next 5 day function")
+        try:
+            done, pending = await asyncio.wait(
+                [today, tomorrow, next_5_day],
+                return_when=asyncio.ALL_COMPLETED
+            )
+            for task in done:
+                name = task.get_name()
+                logging.debug(f"DONE: {name}")
+                exception = task.exception()
+                if isinstance(exception, Exception):
+                    logging.error(f"{name} threw {exception}")
+            for task in pending:
+                task.cancel()
+        except Exception as e:
+            logging.error(e)
 
     async def today(self):
         request = [{
@@ -38,10 +51,9 @@ class Scheduler:
                 }
             }
         }]
-        users = self.database.users.aggregate(request)
-        async for user in users:
-            for group in user["groups"]:
-                logging.debug(user)
+        today = self.database.users.aggregate(request)
+        async for birthday in today:
+            logging.debug(birthday)
 
     async def tomorrow(self):
         request = [{
@@ -61,10 +73,15 @@ class Scheduler:
             for group_id in birthday["groups"]:
                 group = await self.database.groups.find_one({"_id": group_id})
                 for user in filter(lambda i: i['_id'] != birthday["_id"], group["users"]):
-                    if photos.total_count == 0 or not photos:
-                        await self.bot.send_message(user["_id"], (await TOMORROW.render_async(user=birthday, title=group["title"])))
-                    else:
-                        await self.bot.send_photo(user["_id"], photos.photos[0][-1].file_id, caption=(await TOMORROW.render_async(user=birthday, title=group["title"])))
+                    try:
+                        if photos.total_count == 0 or not photos:
+                            await self.bot.send_message(user["_id"], (
+                                await TOMORROW.render_async(user=birthday, title=group["title"])))
+                        else:
+                            await self.bot.send_photo(user["_id"], photos.photos[0][-1].file_id, caption=(
+                                await TOMORROW.render_async(user=birthday, title=group["title"])))
+                    except Exception as err:
+                        logging.error(err)
 
     async def next_5_day(self):
         request = [{
@@ -84,9 +101,12 @@ class Scheduler:
             for group_id in birthday["groups"]:
                 group = await self.database.groups.find_one({"_id": group_id})
                 for user in filter(lambda i: i['_id'] != birthday["_id"], group["users"]):
-                    if photos.total_count == 0:
-                        await self.bot.send_message(user["_id"], (
-                            await NEXT_5_DAY.render_async(user=birthday, title=group["title"])))
-                    else:
-                        await self.bot.send_photo(user["_id"], photos.photos[0][-1].file_id, caption=(
-                            await NEXT_5_DAY.render_async(user=birthday, title=group["title"])))
+                    try:
+                        if photos.total_count == 0:
+                            await self.bot.send_message(user["_id"], (
+                                await NEXT_5_DAY.render_async(user=birthday, title=group["title"])))
+                        else:
+                            await self.bot.send_photo(user["_id"], photos.photos[0][-1].file_id, caption=(
+                                await NEXT_5_DAY.render_async(user=birthday, title=group["title"])))
+                    except Exception as err:
+                        logging.error(err)
