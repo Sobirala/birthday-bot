a3e4b9cde246187a10aab0820aebcc170645ae1a
Kulunchick <nikolikrasovski@gmail.com>
2023-02-03 23:52:20 +0200

    UPD: Remove extra

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,13 +2,9 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage
-from motor.motor_asyncio import AsyncIOMotorClient
 from redis import asyncio as aioredis
 
 from bot.config import Settings
-from bot.handlers import inGroup, inPrivate, exceptions
-from bot.middlewares.globalVariables import GlobalVariables
-from bot.scheduler import Scheduler
 from bot.utils import logs
 
 
@@ -27,18 +23,6 @@ async def main():
     ))
     bot = Bot(token=config.TOKEN.get_secret_value(), parse_mode="HTML")
     dp = Dispatcher(storage=storage)
-    client = AsyncIOMotorClient(config.MONGO_URL.get_secret_value())
-    db = client.birthdays
-
-    scheduler = Scheduler(bot, db)
-    await scheduler.start()
-
-    dp.message.middleware(GlobalVariables(db, config))
-    dp.callback_query.middleware(GlobalVariables(db, config))
-
-    dp.include_router(inPrivate.router)
-    dp.include_router(inGroup.router)
-    dp.include_router(exceptions.router)
 
     await dp.start_polling(bot)
 
