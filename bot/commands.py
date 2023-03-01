5d309ff2c6cd5055be593cd5550daa5b11dd5657
Kulunchick <nikolikrasovski@gmail.com>
2023-03-01 18:39:11 +0200

    UPD: Added commands description

---
+++ bot/commands.py
@@ -0,0 +1,50 @@
+from aiogram import Bot
+from aiogram.types import (
+    BotCommand,
+    BotCommandScopeAllChatAdministrators,
+    BotCommandScopeAllGroupChats,
+    BotCommandScopeAllPrivateChats,
+)
+
+
+async def set_bot_commands(bot: Bot) -> None:
+    uk_commands = (
+        (
+            (
+                BotCommand(
+                    command="start", description="Вивести стартове повідомлення"
+                ),
+                BotCommand(
+                    command="calendar",
+                    description="Переглянути всі дні народження групи",
+                ),
+                BotCommand(command="reset", description="Змінити дані про себе"),
+                BotCommand(command="removeme", description="Видалити дані про себе"),
+            ),
+            BotCommandScopeAllPrivateChats(),
+        ),
+        (
+            (
+                BotCommand(
+                    command="start",
+                    description="Вивести повідомлення з пропозицією додати ДН до календаря групи",
+                ),
+            ),
+            BotCommandScopeAllGroupChats(),
+        ),
+        (
+            (
+                BotCommand(
+                    command="start",
+                    description="Вивести повідомлення з пропозицією додати ДН до календаря групи",
+                ),
+                BotCommand(
+                    command="collect",
+                    description="Увімкнути чи вимкнути збори грошей (тільки для адміністраторів групи)",
+                ),
+            ),
+            BotCommandScopeAllChatAdministrators(),
+        ),
+    )
+    for commands, scope in uk_commands:
+        await bot.set_my_commands(commands=commands, scope=scope, language_code="uk")
