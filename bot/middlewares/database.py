d58d0da36d52a01633e79fbabdf18fc663c63f07
Kulunchick <nikolikrasovski@gmail.com>
2023-03-01 18:59:46 +0200

    UPD: Added session maker middleware

--- bot/middlewares/database.py
+++ bot/middlewares/database.py
@@ -1,21 +1,19 @@
-from typing import Callable, Dict, Any, Awaitable
+from typing import Any, Awaitable, Callable, Dict
 
 from aiogram import BaseMiddleware
 from aiogram.types import Update
-from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 
 class SessionMaker(BaseMiddleware):
-    _async_sessionmaker: async_sessionmaker
-
-    def __init__(self, sessionmaker):
+    def __init__(self, sessionmaker: async_sessionmaker[AsyncSession]):
         self._async_sessionmaker = sessionmaker
 
     async def __call__(
-        self,
-        handler: Callable[[Update, Dict[str, Any]], Awaitable[Any]],
-        event: Update,
-        data: Dict[str, Any]
+            self,
+            handler: Callable[[Update, Dict[str, Any]], Awaitable[Any]],
+            event: Update,
+            data: Dict[str, Any]
     ) -> Any:
         async with self._async_sessionmaker() as session:
             async with session.begin():
