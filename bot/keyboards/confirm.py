2255930351d9834b140537a2deed2f37db8dda14
Kulunchick <nikolikrasovski@gmail.com>
2023-03-05 22:22:54 +0200

    UPD: Added confirm keyboard

---
+++ bot/keyboards/confirm.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from fluentogram import TranslatorRunner
+
+
+class ConfirmTypes(Enum):
+    YES = "yes"
+    NO = "no"
+
+
+class SelectConfirm(CallbackData, prefix="confirm"):
+    selected: ConfirmTypes
+
+
+async def confirm_keyboard(i18n: TranslatorRunner) -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    for i in ConfirmTypes:
+        builder.button(
+            text=i18n.confirm(confirm=i.value), callback_data=SelectConfirm(selected=i)
+        )
+    builder.adjust(2)
+    return builder.as_markup()
