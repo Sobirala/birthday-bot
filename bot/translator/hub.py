f29e4cc98806865242161783b7326e2adaad487a
Kulunchick <nikolikrasovski@gmail.com>
2023-03-05 22:14:46 +0200

    UPD: Added translator hub

--- bot/translator/hub.py
+++ bot/translator/hub.py
@@ -6,27 +6,27 @@ class Translator:
     def __init__(self):
         self.t_hub = TranslatorHub(
             {
-                "uk-UA": (
-                    "uk-UA",
-                    "en-US",
+                "uk_UA": (
+                    "uk_UA",
+                    "en_US",
                 ),
-                "en-US": ("en-US",),
+                "en_US": ("en_US",),
             },
             [
                 FluentTranslator(
-                    locale="uk-UA",
+                    locale="uk_UA",
                     translator=FluentBundle.from_files(
-                        "uk-UA", filenames=["../locales/ua.ftl"], use_isolating=False
+                        "uk_UA", filenames=["locales/ua.ftl"], use_isolating=False
                     ),
                 ),
                 FluentTranslator(
-                    locale="en-US",
+                    locale="en_US",
                     translator=FluentBundle.from_files(
-                        "en-US", filenames=["../locales/en.ftl"], use_isolating=False
+                        "en_US", filenames=["locales/en.ftl"], use_isolating=False
                     ),
                 ),
             ],
-            root_locale="uk-UA",
+            root_locale="uk_UA",
         )
 
     def __call__(self, language: str) -> TranslatorRunner:
