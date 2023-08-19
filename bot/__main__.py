4ec148b39fa0976f44d8a157447da6b51fc57cc7
Kulunchick <nikolikrasovski@gmail.com>
2023-08-19 03:33:00 +0300

    UPD: Updated main

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,60 +3,57 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
-from loguru import logger
 from redis import asyncio as aioredis
 from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-from bot.services.google_maps import GoogleMaps
 from bot.commands import set_bot_commands
-from bot.config import Settings
-from bot.handlers.group import router as group_router
-from bot.handlers.private import router as private_router
+from bot.settings import settings
 from bot.keyboards.calendar_widget import Calendar
 from bot.middlewares.database import SessionMaker
 from bot.middlewares.i18n import TranslatorRunnerMiddleware
+from bot.routers import router
+from bot.services.scheduler import Scheduler
 from bot.translator.hub import Translator
-from bot.utils import logs
+from bot.utils.logger import setup_logger
 
 
 async def main():
-    config = Settings()
-    logs.setup(level=config.LOGLEVEL)
+    setup_logger()
     engine = create_async_engine(
         URL.create(
             "postgresql+asyncpg",
-            username=config.POSTGRES_USER,
-            password=config.POSTGRES_PASSWORD.get_secret_value(),
-            host=config.POSTGRES_HOST,
-            port=config.POSTGRES_PORT,
-            database=config.POSTGRES_DB,
+            username=settings.POSTGRES_USER,
+            password=settings.POSTGRES_PASSWORD.get_secret_value(),
+            host=settings.POSTGRES_HOST,
+            port=settings.POSTGRES_PORT,
+            database=settings.POSTGRES_DB,
         )
     )
     async_session = async_sessionmaker(engine, expire_on_commit=False, autoflush=False)
 
     redis = aioredis.Redis(
-        host=config.REDIS_HOST,
-        port=config.REDIS_PORT,
-        username=config.REDIS_USERNAME,
-        password=config.REDIS_PASSWORD.get_secret_value(),
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        username=settings.REDIS_USERNAME,
+        password=settings.REDIS_PASSWORD.get_secret_value(),
         decode_responses=True
     )
-    logger.debug(redis)
+
     storage = RedisStorage(redis=redis)
-    bot = Bot(token=config.TOKEN.get_secret_value(), parse_mode=ParseMode.HTML)
+    bot = Bot(token=settings.TOKEN.get_secret_value(), parse_mode=ParseMode.HTML)
     dp = Dispatcher(storage=storage)
 
-    GoogleMaps(api_key=config.GOOGLE_TOKEN.get_secret_value())
+    scheduler = Scheduler(bot, async_session)
+    await scheduler.start()
 
-    await Calendar.register_widget(dp)
+    Calendar.register_widget(dp)
     translator = Translator()
 
     dp.update.middleware(SessionMaker(sessionmaker=async_session))
     dp.update.middleware(TranslatorRunnerMiddleware(translator=translator))
 
-    dp.include_router(private_router)
-    dp.include_router(group_router)
+    dp.include_router(router)
 
     await set_bot_commands(bot)
 
